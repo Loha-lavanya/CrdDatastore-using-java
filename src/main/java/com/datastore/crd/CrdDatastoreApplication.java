81a7547e82d0b8a78c291d3f41c77d9213a96a54
Loha-lavanya <66911280+Loha-lavanya@users.noreply.github.com>
2020-12-13 22:07:11 +0530

    Update CrdDatastoreApplication.java

--- src/main/java/com/datastore/crd/CrdDatastoreApplication.java
+++ src/main/java/com/datastore/crd/CrdDatastoreApplication.java
@@ -1,1 +1,14 @@
+package com.datastore.crd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrdDatastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrdDatastoreApplication.class, args);
+	}
+
+}
 
